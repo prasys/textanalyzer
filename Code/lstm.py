ac8a95d5edf192f642fdbbc489cbe08aa7c2f404
prasys <pradeesh@prasys.info>
2020-01-28 11:13:24 +1300

    Update lstm.py

--- Code/lstm.py
+++ Code/lstm.py
@@ -87,47 +87,47 @@ print(weights)
 
 
 
-#model = Sequential()
-#model.add(Embedding(input_dim=max_features, output_dim=embed_dim,input_length = X.shape[1]))
-#model.add(LSTM(embed_dim, dropout=0.2, recurrent_dropout=0.2))
-#model.add(Dense(32,activation='tanh'))
-#model.add(Dense(2,activation='softmax'))
-test = UniversalEmbedding(x)
-print(type(test))
-input_text = Input(shape=(1,), dtype=tf.string)
-embedding = Lambda(UniversalEmbedding, output_shape=(512, ))(input_text)
-#lgbt = LSTM(32,dropout=True, recurrent_dropout=0.4) (embedding)
-#lstm_encode = LSTM(16,recurrent_dropout=0.4) (lgbt)
-dense = Dense(256, activation='relu')(embedding)
-pred = Dense(2, activation='softmax')(dense)
-model = Model(inputs=[input_text], outputs=pred)
-model.compile(loss = 'categorical_crossentropy', optimizer='adam',metrics = ['accuracy'])
-print(model.summary())
-X_train, X_test, Y_train, Y_test = train_test_split(x,Y, test_size = 0.30, random_state = 42)
-batch_size = 64
-#model.fit(X_train, Y_train, epochs = 10, batch_size=batch_size, verbose = 2, class_weight=weights)
-#validation_size = 1500
+# #model = Sequential()
+# #model.add(Embedding(input_dim=max_features, output_dim=embed_dim,input_length = X.shape[1]))
+# #model.add(LSTM(embed_dim, dropout=0.2, recurrent_dropout=0.2))
+# #model.add(Dense(32,activation='tanh'))
+# #model.add(Dense(2,activation='softmax'))
+# test = UniversalEmbedding(x)
+# print(type(test))
+# input_text = Input(shape=(1,), dtype=tf.string)
+# embedding = Lambda(UniversalEmbedding, output_shape=(512, ))(input_text)
+# #lgbt = LSTM(32,dropout=True, recurrent_dropout=0.4) (embedding)
+# #lstm_encode = LSTM(16,recurrent_dropout=0.4) (lgbt)
+# dense = Dense(256, activation='relu')(embedding)
+# pred = Dense(2, activation='softmax')(dense)
+# model = Model(inputs=[input_text], outputs=pred)
+# model.compile(loss = 'categorical_crossentropy', optimizer='adam',metrics = ['accuracy'])
+# print(model.summary())
+# X_train, X_test, Y_train, Y_test = train_test_split(x,Y, test_size = 0.30, random_state = 42)
+# batch_size = 64
+# #model.fit(X_train, Y_train, epochs = 10, batch_size=batch_size, verbose = 2, class_weight=weights)
+# #validation_size = 1500
 
-with tf.Session() as session:
-    K.set_session(session)
-    session.run(tf.global_variables_initializer())  
-    session.run(tf.tables_initializer())
-    tweet_embeddings = embed(x.tolist())
-    tweet_embeddings = tweet_embeddings.eval()
-    #session.run(tweet_embeddings)
-    #tweet_embeddings = np.array(tweet_embeddings)
-    print("FUCKING SHAPE IS ", tweet_embeddings.shape)
-    print(tweet_embeddings)
+# with tf.Session() as session:
+#     K.set_session(session)
+#     session.run(tf.global_variables_initializer())  
+#     session.run(tf.tables_initializer())
+#     tweet_embeddings = embed(x.tolist())
+#     tweet_embeddings = tweet_embeddings.eval()
+#     #session.run(tweet_embeddings)
+#     #tweet_embeddings = np.array(tweet_embeddings)
+#     print("FUCKING SHAPE IS ", tweet_embeddings.shape)
+#     print(tweet_embeddings)
    
 
-    np.save('embeddings.npy',tweet_embeddings)
+#     np.save('embeddings.npy',tweet_embeddings)
 
-    #history = model.fit(X_train, Y_train, epochs=25, batch_size=32, class_weight=weights)
-   # score,acc = model.evaluate(X_test, Y_test, verbose = 2, batch_size = batch_size)
-    #model.save_weights("model.h5")
-    #print("Saved model to disk")
+#     #history = model.fit(X_train, Y_train, epochs=25, batch_size=32, class_weight=weights)
+#    # score,acc = model.evaluate(X_test, Y_test, verbose = 2, batch_size = batch_size)
+#     #model.save_weights("model.h5")
+#     #print("Saved model to disk")
 
-np.save('embeddings.npy',tweet_embeddings)
+# np.save('embeddings.npy',tweet_embeddings)
 
 
 #with tf.Session() as session:
@@ -150,12 +150,12 @@ np.save('embeddings.npy',tweet_embeddings)
 #print("acc: %.2f" % (acc))
 
 
-if ClassifyIt is True:
-	df2 = read_csv2("test_noannotations.csv")
-	df2['pred'] = df2['Comment'].apply(predictText)
-	df2.to_csv('lstm_output.csv',index=False)
-	#df3 = pd.DataFrame(tester)
-	#df3.to_csv('output_lstm.csv',index=False)
+# if ClassifyIt is True:
+# 	df2 = read_csv2("test_noannotations.csv")
+# 	df2['pred'] = df2['Comment'].apply(predictText)
+# 	df2.to_csv('lstm_output.csv',index=False)
+# 	#df3 = pd.DataFrame(tester)
+# 	#df3.to_csv('output_lstm.csv',index=False)
 
 
 
